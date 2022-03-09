f04339e17ce3ccf67b3282937ccbe0cf358503ea
jaywangcc126112 <1514803091@qq.com>
2022-03-09 10:02:39 +0800

    Site updated: 2022-03-09 10:02:38

--- js/share.js
+++ js/share.js
@@ -26,10 +26,9 @@
         '<div id="' + id + '" class="article-share-box">',
           '<input class="article-share-input" value="' + url + '">',
           '<div class="article-share-links">',
-            // '<a href="//twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            // '<a href="//www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="https://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?title=' + title + '&url=' + encodedUrl + '&sharesource=qzone&summary=你的分享描述信息' + '" class="article-share-qq" target="_blank" title="qq"></a>',
-            // '<a href="' + qrcode_img + '" class="article-share-qrcode" target="_blank" title="QR code"></a>',
+            '<a href="//twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+            '<a href="//www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
+            '<a href="' + qrcode_img + '" class="article-share-qrcode" target="_blank" title="QR code"></a>',
             '<div class="qrcode"><img src=' + qrcode_img + '></div>',
           '</div>',
         '</div>'
